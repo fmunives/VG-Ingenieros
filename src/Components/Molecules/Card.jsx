44736fcc624343c6087949663f4a837a552b3606
Freddy Munive <munivef@yahoo.com>
2019-10-24 22:22:39 -0500

    modificando card

--- src/Components/Molecules/Card.jsx
+++ src/Components/Molecules/Card.jsx
@@ -3,14 +3,19 @@ import { Link } from 'react-router-dom';
 
 const Card = ({ image, image_teacher, teacher, price, title, id }) => {
   return (
-    <article className='card'>
+    <article className='card s-radius s-border '>
       <div className='img-container s-ratio-16-9 s-radius-tr s-radius-tl'>
         <Link to={`/cursos/${id}`}>
           <img src={image} alt={title} />
         </Link>
       </div>
-      <div className='card__data s-border s-radius-br s-radius-bl s-pxy-2'>
-        <h3 className='t5 s-mb-2 s-center'>{title}</h3>
+      <div className='card__data s-pxy-2 img-percentage s-radius-br s-radius-bl'>
+        <h3 className='t5 s-mb-2 s-center'>
+          <Link className='color dark-color' to={`/cursos/${id}`}>
+            {title}
+          </Link>
+        </h3>
+        <div className='s-mb-1'></div>
         <div className='s-mb-2 s-main-center'>
           <div className='card__teacher s-cross-center'>
             <div className='card__avatar s-mr-1'>
